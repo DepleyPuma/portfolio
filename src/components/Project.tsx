ca525c4027ce3f13775862665f4b23b869f30875
DepleyPuma <szymon.szarek1@wp.pl>
2025-07-06 10:41:20 +0200

    project development

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,12 +8,8 @@ import { Dispatch, SetStateAction, useState } from 'react';
 export const Project = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<div>
-			<div className='w-full max-w-4xl overflow-hidden'>
-				<h3 className='text-2xl font-bold text-white'>Project Name</h3>
-				{/* <div className='pointer-events-none absolute inset-x-0 -bottom-2 h-1/2 w-[110%] bg-gradient-to-t from-[var(--website-bg-color)] to-transparent'></div> */}
-			</div>
-			<div>
+		<div className='relative w-full max-w-2xl mx-auto'>
+			<div className='w-full h-[230px] md:h-[400px] mx-auto'>
 				<Image fill src='/test.webp' alt='test' />
 			</div>
 			{/* <button
@@ -22,18 +18,18 @@ export const Project = () => {
 			>
 				Open Modal
 			</button> */}
+			<div className='absolute bottom-0 z-10'>
+				<h3 className='text-2xl md:text-4xl p-4 font-extrabold italic text-white'>Project Name</h3>
+				{/* <div className='pointer-events-none absolute inset-x-0 -bottom-2 h-1/2 w-[110%] bg-gradient-to-t from-[var(--website-bg-color)] to-transparent'></div> */}
+			</div>
 			<SpringModal isOpen={isOpen} setIsOpen={setIsOpen} />
+
+			<div className='pointer-events-none absolute inset-x-0 -bottom-2 h-1/2 w-[110%] bg-gradient-to-t from-[var(--website-bg-color)] to-transparent'></div>
 		</div>
 	);
 };
 
-const SpringModal = ({
-	isOpen,
-	setIsOpen,
-}: {
-	isOpen: boolean;
-	setIsOpen: Dispatch<SetStateAction<boolean>>;
-}) => {
+const SpringModal = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>> }) => {
 	return (
 		<AnimatePresence>
 			{isOpen && (
@@ -42,15 +38,13 @@ const SpringModal = ({
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
 					onClick={() => setIsOpen(false)}
-					className='bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer'
-				>
+					className='bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer'>
 					<motion.div
 						initial={{ scale: 0, rotate: '12.5deg' }}
 						animate={{ scale: 1, rotate: '0deg' }}
 						exit={{ scale: 0, rotate: '0deg' }}
 						onClick={e => e.stopPropagation()}
-						className='bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden'
-					>
+						className='bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden'>
 						<CircleAlert className='text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24' />
 						<div className='relative z-10'>
 							<div className='bg-white w-16 h-16 mb-2 rounded-full text-3xl text-indigo-600 grid place-items-center mx-auto'>
@@ -58,20 +52,18 @@ const SpringModal = ({
 							</div>
 							<h3 className='text-3xl font-bold text-center mb-2'>One more thing!</h3>
 							<p className='text-center mb-6'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aperiam
-								vitae, sapiente ducimus eveniet in velit.
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aperiam vitae, sapiente ducimus eveniet in
+								velit.
 							</p>
 							<div className='flex gap-2'>
 								<button
 									onClick={() => setIsOpen(false)}
-									className='bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded'
-								>
+									className='bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded'>
 									Nah, go back
 								</button>
 								<button
 									onClick={() => setIsOpen(false)}
-									className='bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded'
-								>
+									className='bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded'>
 									Understood!
 								</button>
 							</div>
