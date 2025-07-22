3f537928afad732f0835cf5a611c1c7c91e91049
DepleyPuma <szymon.szarek1@wp.pl>
2025-07-22 20:14:01 +0200

    about sub page

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,10 +1,33 @@
 import { StepsType } from '@/lib/steps';
 import { useTranslations } from 'next-intl';
+import { useRef } from 'react';
+import { motion, useInView } from 'motion/react';
 
-export function Step({ index, title, text, translate }: StepsType) {
+type StepTypeProps = StepsType & { delayIndex: number };
+
+export function Step({ index, title, text, translate, delayIndex }: StepTypeProps) {
+	const ref = useRef(null);
 	const t = useTranslations('services');
+
+	const isInView = useInView(ref, { once: true, margin: '-100px 0px' });
+
+	const variants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0 },
+	};
+
 	return (
-		<div className='flex flex-col items-start w-full lg:max-w-[33svw] text-white'>
+		<motion.div
+			ref={ref}
+			variants={variants}
+			initial='hidden'
+			animate={isInView ? 'visible' : 'hidden'}
+			transition={{
+				duration: 0.5,
+				ease: 'easeInOut',
+				delay: delayIndex * 0.25,
+			}}
+			className='flex flex-col items-start w-full lg:max-w-[33svw] text-white'>
 			<span className='mb-4 text-lg'>{index}</span>
 
 			<hr className='border-0 h-[1px] w-full bg-muted-foreground' />
@@ -12,6 +35,6 @@ export function Step({ index, title, text, translate }: StepsType) {
 			<h3 className='py-8 text-2xl'>{t(`${translate}.${title}`)}</h3>
 
 			<p>{t(`${translate}.${text}`)}</p>
-		</div>
+		</motion.div>
 	);
 }
--- src/view/ICanHelpYou.tsx
+++ src/view/ICanHelpYou.tsx
@@ -2,11 +2,21 @@
 import { AnimatedText } from '@/components/animated/AnimatedText/AnimatedText';
 import { Step } from '@/components/Step';
 import { stepsArr } from '@/lib/steps';
-import { motion, Variants } from 'motion/react';
+import { motion, useInView, Variants } from 'motion/react';
+import { useRef } from 'react';
 
 export function ICanHelpYou() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true, margin: '-100px 0px' });
+
+	const variants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0 },
+	};
+
 	const dotVariants: Variants = {
 		jump: {
+			opacity: 1,
 			y: -10,
 			transition: {
 				duration: 0.8,
@@ -25,33 +35,34 @@ export function ICanHelpYou() {
 					duration={1}
 					delay={0.5}
 					as='h2'
-					className='text-4xl xl:text-5xl font-extrabold italic mb-16 text-white'
-				>
+					className='text-4xl xl:text-5xl font-extrabold italic mb-16 text-white'>
 					I can help you with
 				</AnimatedText>
 				<motion.div
-					animate='jump'
-					transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
-					className='flex justify-center items-center gap-1'
-				>
+					ref={ref}
+					variants={variants}
+					initial='hidden'
+					animate={isInView ? 'visible' : 'hidden'}
+					transition={{
+						duration: 0.5,
+						ease: 'easeInOut',
+						delay: 0.6,
+					}}>
 					<motion.div
-						className='w-2.5 h-2.5 rounded-full mt-8 bg-white'
-						variants={dotVariants}
-					/>
-					<motion.div
-						className='w-2.5 h-2.5 rounded-full mt-8 bg-white'
-						variants={dotVariants}
-					/>
-					<motion.div
-						className='w-2.5 h-2.5 rounded-full mt-8 bg-white'
-						variants={dotVariants}
-					/>
+						initial='hidden'
+						animate='jump'
+						transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
+						className='flex justify-center items-center gap-1'>
+						<motion.div className='w-2.5 h-2.5 rounded-full mt-8 bg-white' variants={dotVariants} />
+						<motion.div className='w-2.5 h-2.5 rounded-full mt-8 bg-white' variants={dotVariants} />
+						<motion.div className='w-2.5 h-2.5 rounded-full mt-8 bg-white' variants={dotVariants} />
+					</motion.div>
 				</motion.div>
 			</div>
 
 			<div className='flex flex-col justify-center items-start gap-14 lg:flex-row'>
-				{stepsArr.map(step => (
-					<Step key={step.index} {...step} />
+				{stepsArr.map((step, index) => (
+					<Step key={step.index} {...step} delayIndex={index} />
 				))}
 			</div>
 		</section>
